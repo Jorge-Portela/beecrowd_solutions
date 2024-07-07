982938500dbed8afa136ac0889c28a1bdbb142bf
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-06 23:24:24 -0300

    Change the folder name  from 'uri_1003' to 'bee_10033'

--- beginner/uri_1003/src/bee_1003/application/SimpleSum.java
+++ beginner/uri_1003/src/bee_1003/application/SimpleSum.java
@@ -1,4 +1,4 @@
-package uri_1003.application;
+package bee_1003.application;
 /*
 * Read two integer values, in this case, the variables A and B. After this, calculate the sum between them
 *  and assign it to the variable SOMA. Write the value of this variable.
