7493351acc4116d2ca7e0d00bbd060aa478bd9f9
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-11 23:37:33 -0300

    Fixed the header description of bee_1005 ' Average 1' Challenger

--- beginner/uri_1003/src/bee_1005/Average1.java
+++ beginner/uri_1003/src/bee_1005/Average1.java
@@ -1,7 +1,7 @@
 package bee_1005;
 
 /**
- * ead two floating points' values of double precision A and B, corresponding to two student's grades. After this,
+ * Read two floating points' values of double precision A and B, corresponding to two student's grades. After this,
  * calculate the student's average, considering that grade A has weight 3.5 and B has weight 7.5. Each grade can be
  * from zero to ten, always with one digit after the decimal point. Don’t forget to print the end of line after the
  * result, otherwise you will receive “Presentation Error”. Don’t forget the space before and after the equal sign.
@@ -20,6 +20,6 @@ package bee_1005;
 
 public class Average1 {
     public static void main(String[] args) {
-        
+
     }
 }
