78136138be9403f642f3144a1e44f494d75f852c
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-11 23:46:35 -0300

    Added the 'B' double variable that receive the second float numeber

--- beginner/uri_1003/src/bee_1005/Average1.java
+++ beginner/uri_1003/src/bee_1005/Average1.java
@@ -26,6 +26,7 @@ public class Average1 {
         Scanner reader = new Scanner(System.in);
 
         double A = reader.nextDouble();
+        double B = reader.nextDouble();
 
         reader.close();
 
