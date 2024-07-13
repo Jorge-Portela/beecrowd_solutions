305fd12f05693c8ab4ad08e8daf39c7a7b6a4327
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-12 23:42:32 -0300

    Added 'average' double type variable that calculate the average grade os student wich there are weight 3 to 'A' grade, weight 3 to 'B' grade and weight to 'C' grade

--- beginner/uri_1003/src/bee_1006/Average2.java
+++ beginner/uri_1003/src/bee_1006/Average2.java
@@ -30,6 +30,10 @@ public class Average2 {
         double B = reader.nextDouble();
         double C = reader.nextDouble();
 
+        double average = ((A * 2) + (B * 3) + (C + 5)) /  (2+3+5);
+
+
+
          reader.close();
     }
 }
