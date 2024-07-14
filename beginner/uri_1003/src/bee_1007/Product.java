aa300c3adc6fda5ff04e1b5ea76a56fa26649f18
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-13 23:38:09 -0300

    Added the header of challenger 1007

---
+++ beginner/uri_1003/src/bee_1007/Product.java
@@ -0,0 +1,19 @@
+package bee_1007;
+
+/**
+ * Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the
+ * product of C and D (A * B - C * D).
+ *
+ *   Input
+ *   The input file contains 4 integer values.
+ * */
+
+import java.util.Locale;
+
+
+public class Product {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+    }
+}
