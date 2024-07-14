21f3df7cc8908cb73792716863089db1006de2dd
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-14 00:35:05 -0300

    Added the import of Scanner class to receive the inputs of 1008 challenger

--- beginner/uri_1003/src/bee_1008/Employeer1.java
+++ beginner/uri_1003/src/bee_1008/Employeer1.java
@@ -15,12 +15,15 @@ package bee_1008;
  * */
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Employeer1 {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
 
-        
+        reader.close();
+
 
     }
 }
