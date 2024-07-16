1578e5a3341d4b02fd0d9fc134efb042cbcfea48
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-15 22:57:39 -0300

    Added the header description of 1009 challenger

---
+++ beginner/uri_1003/src/bee_1009/SalaryWithBonus.java
@@ -0,0 +1,16 @@
+package bee_1009;
+
+/**
+ * Make a program that reads a seller's name, the fixed salary and the sale's total made  in the month (in money).
+ * Considering that this seller receives 15% over all products sold, write the final salary
+ * (total) of this seller at the end of the month , with two decimal places.
+ *
+ * The input file contains a text (employee's first name), and two double precision values, that are the seller's
+ * salary and the total value sold by him/her.
+ * */
+
+public class SalaryWithBonus {
+    public static void main(String[] args) {
+        
+    }
+}
