da293ab33c47f2fb3f23258a7c3e6fb53dde2e04
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-19 23:19:16 -0300

    Added Scanner class import to receive the inmput of challenger 1014

--- beginner/uri_1003/src/bee_1014/Consumption.java
+++ beginner/uri_1003/src/bee_1014/Consumption.java
@@ -15,10 +15,13 @@ package bee_1014;
  * */
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Consumption {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
         
+        reader.close();
     }
 }
