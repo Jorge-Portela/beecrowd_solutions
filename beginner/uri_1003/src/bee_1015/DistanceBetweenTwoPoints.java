05c12fe4db83639329aa739beda24cbb69b1d419
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-07-20 22:10:46 -0300

    Added the 'Scanner' class with 'reader' object instanced to receive the input of challenger 1015

--- beginner/uri_1003/src/bee_1015/DistanceBetweenTwoPoints.java
+++ beginner/uri_1003/src/bee_1015/DistanceBetweenTwoPoints.java
@@ -15,10 +15,13 @@ package bee_1015;
  * */
 
 import java.util.Locale;
-
+import java.util.Scanner;
 public class DistanceBetweenTwoPoints {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
+        Scanner reader = new Scanner(System.in);
+
+        reader.close();
     }
 }
