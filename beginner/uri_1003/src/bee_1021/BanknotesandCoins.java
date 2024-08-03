d7e14cc13cdf6ab64a4c47e9f46a35d60e90d17c
Jorge  Portela <jorge.felipe.portela@gmail.com>
2024-08-02 22:17:29 -0300

    Added Decimal Fomat to 'twoBanknote'

--- beginner/uri_1003/src/bee_1021/BanknotesandCoins.java
+++ beginner/uri_1003/src/bee_1021/BanknotesandCoins.java
@@ -80,7 +80,7 @@ public class BanknotesandCoins {
         System.out.println(df.format(twentyBanknote)+" nota(s) de R$ 20.00");
         System.out.println(df.format(tenBanknote)+" nota(s) de R$ 10.00");
         System.out.println(df.format(fiveBanknote)+" nota(s) de R$ 50.00");
-        System.out.println(twoBanknote+" nota(s) de R$ 2.00");
+        System.out.println(df.format(twoBanknote)+" nota(s) de R$ 2.00");
 
 
 
